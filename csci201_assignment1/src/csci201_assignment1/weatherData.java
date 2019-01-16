f2d142b6d5c0ade23129665cb3dd8c2e11586383
beiyouzhu <beiyouzhu@10.183.12.63>
2019-01-15 20:59:01 -0800

    ok

--- csci201_assignment1/src/csci201_assignment1/weatherData.java
+++ csci201_assignment1/src/csci201_assignment1/weatherData.java
@@ -1,5 +1,28 @@
 package csci201_assignment1;
 
 public class weatherData {
+	private String city;
+	private int currentTemperature;
+	private int dayLow;
+	private int dayHigh;
+	private float pressure;
+	private float visibility;
+	private float windspeed;
+	private int windDirection;
+	
+	public weatherData(String city, int currentTemperature, int dayLow, int dayHigh, float pressure, float visibility,
+			float windspeed, int windDirection) {
+		this.city = city;
+		this.currentTemperature = currentTemperature;
+		this.dayLow = dayLow;
+		this.dayHigh = dayHigh;
+		this.pressure = pressure;
+		this.visibility = visibility;
+		this.windspeed = windspeed;
+		this.windDirection = windDirection;
+	}
 
+	public String conditionDescription() {
+		
+	}
 }
